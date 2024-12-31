4879fe5e9302e6a2c3df1f6b3ee3b55ceb0b072d
Aryan1158 <aryan.n.yadav@slrtce.in>
2024-12-31 18:20:16 +0530

    userform

---
+++ userform/lib/extensions/extensions.dart
@@ -0,0 +1,12 @@
+extension ExtString on String {
+
+  bool get isValidEmail {
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegExp.hasMatch(this);
+  }
+
+    bool get isPasswprd {
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegExp.hasMatch(this);
+  }
+}
